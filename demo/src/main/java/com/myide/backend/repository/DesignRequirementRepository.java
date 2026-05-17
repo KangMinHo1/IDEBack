519a74768b6ed1873bce34311fdc143692bc5c44
dlgywn2263 <lhj-3988@naver.com>
2026-05-17 13:21:08 +0900

    설계관리 Repository 추가

---
+++ demo/src/main/java/com/myide/backend/repository/DesignRequirementRepository.java
@@ -0,0 +1,16 @@
+package com.myide.backend.repository;
+
+import com.myide.backend.domain.design.DesignRequirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DesignRequirementRepository extends JpaRepository<DesignRequirement, Long> {
+
+    Optional<DesignRequirement> findByUuid(String uuid);
+
+    List<DesignRequirement> findByWorkspace_UuidOrderByCreatedAtAsc(String workspaceUuid);
+
+    boolean existsByUuidAndWorkspace_Uuid(String uuid, String workspaceUuid);
+}
