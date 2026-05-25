ee23d908c3bd4d6da14e276e57ddefdbf15f3f38
dlgywn2263 <lhj-3988@naver.com>
2026-05-25 19:48:55 +0900

    설계 repository ìì삭제

---
+++ demo/src/main/java/com/myide/backend/repository/design/DesignApiSpecRepository.java
@@ -0,0 +1,16 @@
+package com.myide.backend.repository.design;
+
+import com.myide.backend.domain.design.DesignApiSpec;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DesignApiSpecRepository extends JpaRepository<DesignApiSpec, Long> {
+
+    Optional<DesignApiSpec> findByUuid(String uuid);
+
+    List<DesignApiSpec> findByWorkspace_UuidOrderByCreatedAtAsc(String workspaceUuid);
+
+    boolean existsByUuidAndWorkspace_Uuid(String uuid, String workspaceUuid);
+}
